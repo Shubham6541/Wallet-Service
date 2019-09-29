c8741447bcf830106b5f6eeeab9bf5aea96aa5e9
MonikaMahanthappa <monika@mavenhive.in>
2019-09-30 01:32:28 +0530

    Add. Cross origin for login api

--- src/main/java/com/tw/pathashala/api/auth/LoginController.java
+++ src/main/java/com/tw/pathashala/api/auth/LoginController.java
@@ -1,9 +1,11 @@
 package com.tw.pathashala.api.auth;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 class LoginController {
     @GetMapping("/login")
     void login() {
